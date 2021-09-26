393f11b22cc996480e3b01e86bf6bcd006706156
Diavva <Karmasin@inteia.pl>
2021-09-26 12:51:37 +0200

    ćw.4

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,18 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Podaj liczę");
+        int x = 4;
+        int y = 6;
+        int z = 4;
+
+        System.out.println("Operatory porównania: "+ "x = "+x +" y = "+ y +" z = " + z );
+
+        System.out.println("x != y: " + (x != y));
+        System.out.println( "x == z: " + (x==z) );
+        System.out.println("z <= y: " +(z<=y));
+        System.out.println("x >= y: " +(x>=y));
+
+
 
     }
 }
