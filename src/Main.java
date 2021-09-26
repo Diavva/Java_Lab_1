f4776678ddc0239e9860d77a1f9cc12df13415ea
Diavva <Karmasin@inteia.pl>
2021-09-26 13:30:21 +0200

    ćw.6

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,33 @@ public class Main {
         przypisać do niej wartości i wyświetlić */
 
 
-        int [] tab = new int [5];
-        tab [0] = 10;
-        tab [1] = 15;
-        tab [2] = 20;
-        tab [3] = 25;
-        tab [4] = 30;
 
-        System.out.print(tab.length);
+        double[] tab ={11.4,12.7,13.3,14.9,15.01};
+
+
+
+        System.out.println("Tab[0]= " + tab[0]);
+        System.out.println("Tab[1]= " + tab[1]);
+        System.out.println("Tab[2]= " + tab[2]);
+        System.out.println("Tab[3]= " + tab[3]);
+        System.out.println("Tab[4]= " + tab[4]);
+
+        double [] tab2 = new double [5];
+
+        tab2[0] = 23.63;
+        tab2[1] = 44.21;
+        tab2[2] = 51.11;
+        tab2[3] = 13.21;
+        tab2[4] = 11.11;
+
+        System.out.println(" ");
+
+        System.out.println("Tab2[0] " + tab2[0]);
+        System.out.println("Tab2[1] " + tab2[1]);
+        System.out.println("Tab2[2] " + tab2[2]);
+        System.out.println("Tab2[3] " + tab2[3]);
+        System.out.println("Tab2[4] " + tab2[4]);
+
 
 
 
