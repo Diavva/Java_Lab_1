073469db5944e91c1064b17ed9f21b9c85c511e6
Diavva <Karmasin@inteia.pl>
2021-09-26 09:55:10 +0200

    ćw.3 część 1

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,25 @@ public class Main {
         *   wynik wyświetlić w konsoli programu
         */
 
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.print("Podaj pierwsze imię: ");
+        String imie = scan.next();
+
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scan.next();
+
+        System.out.print("" +
+                "Podaj wiek: ");
+
+        int wiek = scan.nextInt();
+
+        System.out.print("Podaj numer indeksu:");
+        int indeks = scan.nextInt();
+
+        System.out.printf("Imię i Nazwisko:  %s %s, Wiek: %d, Numer indeksu: %d.\n ", imie, nazwisko, wiek, indeks );
+
+
     }
 }
