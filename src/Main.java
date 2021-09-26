36eaff359429a7e8add36c828f4d226a8ba47799
Diavva <Karmasin@inteia.pl>
2021-09-26 12:26:05 +0200

    ćw.5

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,18 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+
+        int [] tab = new int [5];
+        tab [0] = 10;
+        tab [1] = 15;
+        tab [2] = 20;
+        tab [3] = 25;
+        tab [4] = 30;
+
+        System.out.print(tab.length);
+
+
+
+
     }
 }
